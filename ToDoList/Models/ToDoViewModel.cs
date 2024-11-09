7118fa1a69b1979db78dd20efdfcb1d11eb24b11
Tyler-Breithaupt <breith61@mail.nmc.edu>
2024-11-09 16:52:22 -0500

    Add project files.

---
+++ ToDoList/Models/ToDoViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoList.Models
+{
+    public class ToDoViewModel
+    {
+        public Filters Filters { get; set; } = new Filters("all-all-all"); 
+        public List<Status> Statuses { get; set; } = new List<Status>(); 
+        public List<Category> Categories { get; set; } = new List<Category>(); 
+        public Dictionary<string, string> DueFilters { get; set; } = new Dictionary<string, string>();
+        public List<ToDo> Tasks { get; set; } = new List<ToDo>(); 
+        public ToDo CurrentTask { get; set; } = new ToDo(); // used for Add
+    }
+}
